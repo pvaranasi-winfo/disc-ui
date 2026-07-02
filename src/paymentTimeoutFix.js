0a03a0bbcb9e424527be261a35263776333e6916
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-07-02 16:39:23 +0530

    feat: Add placeholder for payment timeout fix [SAM1-176]

---
+++ src/paymentTimeoutFix.js
@@ -0,0 +1,6 @@
+/*
+ * TODO: Implement a mechanism to increase the timeout for EU-routed payments to 60 seconds.
+ * The original issue describes a 30-second timeout affecting 8% of EU transactions.
+ * This file is a placeholder as the relevant backend code (StripeGateway.java, PaymentService.java)
+ * was not found in this repository (pvaranasi-winfo/disc-ui).
+ */
