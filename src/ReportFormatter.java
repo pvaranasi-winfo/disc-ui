c739de2ef818db5fc2a8cb77ec888ce79126e87e
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-07-17 13:50:18 +0530

    fix: Add TODO for ReportFormatter.java [SAM1-158]

---
+++ src/ReportFormatter.java
@@ -0,0 +1,4 @@
+// TODO: The ReportFormatter.java file was not found in this repository.
+// According to JIRA SAM1-158, the fix should be applied in ReportFormatter.java line 203:
+// Change: DateTimeFormatter.ofPattern('MM/dd/yyyy')
+// To: DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(userLocale)
