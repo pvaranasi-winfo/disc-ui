90ef63daf80ad6af0d838609750d0040fc780f7e
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-07-21 15:36:35 +0530

    feat: Add placeholder for date formatting [SAM1-158]

---
+++ src/utils/DateFormatter.js
@@ -0,0 +1,5 @@
+// TODO: Implement date formatting logic as per JIRA SAM1-158
+// The original JIRA ticket mentioned 'ReportFormatter.java' but this is a JavaScript/TypeScript repository.
+// Further investigation is needed to locate the correct file for date formatting.
+// The fix should involve changing date formatting from MM/DD/YYYY to DD/MM/YYYY for EU users,
+// potentially using a locale-aware formatting function.
