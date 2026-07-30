55b0c6ff370497cbbea6204be259f2ed3fc947a9
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-07-30 10:46:59 +0530

    feat: Add placeholder for date formatting fix [SAM1-167]

---
+++ src/reports/ReportFormatter.js
@@ -0,0 +1,7 @@
+// TODO: This file is a placeholder. The actual fix for SAM1-167 is expected to be in a Java backend service.
+// The JIRA ticket mentions ReportFormatter.java line 203, changing:
+//   DateTimeFormatter.ofPattern('MM/dd/yyyy')
+// to:
+//   DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(userLocale)
+// This repository (disc-ui) is a UI repository and does not contain Java files.
+// A backend service responsible for generating reports should be investigated for the fix.
