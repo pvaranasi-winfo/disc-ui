110b3679d0fa5347a34698bf674edfc34ea7e6ae
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-07-30 09:31:34 +0530

    feat: Add placeholder for date formatting fix [SAM1-173]

--- src/services/ReportFormatter.java
+++ src/services/ReportFormatter.java
@@ -1,6 +1,7 @@
-// TODO: This file is a placeholder. The actual ReportFormatter.java is likely in a backend service.
-// The fix suggested in JIRA SAM1-173 is to change:
+
+// TODO: This file is a placeholder. The actual fix for SAM1-173 needs to be applied in the backend reporting service.
+// The JIRA ticket suggests changing:
 //   DateTimeFormatter.ofPattern('MM/dd/yyyy')
 // to:
 //   DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(userLocale)
-// This change needs to be applied in the correct backend repository.
+// in ReportFormatter.java line 203.
