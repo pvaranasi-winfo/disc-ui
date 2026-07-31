9e3310e3d8a7c974978ed8d5984f9c5c8e27076b
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-07-31 09:17:41 +0530

    feat: Add placeholder for payment timeout fix [SAM1-157]

---
+++ src/payment_fix.js
@@ -0,0 +1,3 @@
+// TODO: Increase payment timeout to 60000ms to address SAM1-157
+// The relevant files (StripeGateway.java, PaymentService.java) were not found in this repository.
+// This is a placeholder for the fix.
