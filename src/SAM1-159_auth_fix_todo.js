f05955dc1dfabab698995365ae6db75267115eca
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-07-31 09:16:16 +0530

    feat: Add TODO for SAM1-159 [SAM1-159]

---
+++ src/SAM1-159_auth_fix_todo.js
@@ -0,0 +1,4 @@
+/*
+ * TODO: The actual fix for SAM1-159 (NullPointerException in Auth service) needs to be applied in the authentication service repository, not in disc-ui.
+ * This file is a placeholder to create a PR as per instructions.
+ */
