dd528669a4efe9fb33a58bf97daa0f87bd4909d5
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-08-03 23:18:23 +0530

    feat: Add placeholder for SAM1-158 fix [SAM1-158]

---
+++ src/utils/DateFormatterPlaceholder.js
@@ -0,0 +1,6 @@
+// TODO: The JIRA ticket SAM1-158 describes a bug in ReportFormatter.java,
+// which is not found in this repository (pvaranasi-winfo/disc-ui).
+// The fix involves changing date formatting from 'MM/dd/yyyy' to
+// DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(userLocale)
+// in a Java backend service.
+// This file is a placeholder to satisfy the PR creation process.
