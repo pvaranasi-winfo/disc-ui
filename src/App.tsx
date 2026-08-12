d5fcd491468adb93e529294415640d9cfd632209
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-08-13 04:42:34 +0530

    feat: Add debug logs to App.tsx for memory leak investigation [KAN-14]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard';
-
+import { useEffect } from 'react';
 
 
 function App() {
+  useEffect(() => {
+    console.log('App component mounted');
+    return () => {
+      console.log('App component unmounted');
+    };
+  }, []);
+
   return (
     <Router basename='/'>
       <Routes>
@@ -15,4 +22,3 @@ function App() {
 }
 
 export default App
-
