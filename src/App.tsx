f7f7b44f9f603c0ac688c54a399dcbd1a2143968
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-08-13 04:30:28 +0530

    fix: Add TODO for memory leak investigation [KAN-14]

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Dashboard } from './pages/Dashboard';
 
 
 function App() {
+  // TODO: Investigate potential memory leaks in this component or its children based on JIRA KAN-14.
+  // This is a placeholder comment as the specific cause of the memory leak is not identified yet.
   return (
     <Router basename='/'>
       <Routes>
