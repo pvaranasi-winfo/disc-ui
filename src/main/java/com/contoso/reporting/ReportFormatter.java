7970037966505171f5d012534171579bbf3b605c
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-08-12 18:07:51 +0530

    feat: Add placeholder for ReportFormatter.java [KAN-8]

---
+++ src/main/java/com/contoso/reporting/ReportFormatter.java
@@ -0,0 +1,6 @@
+// TODO: Implement date formatting logic as per JIRA KAN-8
+// The auto-generated monthly sales report (reports.sales_summary_monthly) is displaying dates in American format (MM/DD/YYYY) instead of the European format (DD/MM/YYYY) that our UK and EU teams require.
+// Fix suggestion: In ReportFormatter.java line 203, change:
+//   DateTimeFormatter.ofPattern('MM/dd/yyyy')
+// to:
+//   DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(userLocale)
