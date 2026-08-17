ad92cf383be06c5afa89c313d0e16df44259bb66
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-08-17 08:32:55 +0530

    feat: Add placeholder for KAN-21 fix

---
+++ EmailQueueProcessor.java
@@ -0,0 +1,5 @@
+/*
+TODO: The file EmailQueueProcessor.java was not found in this repository. 
+This is a placeholder to create a PR for JIRA KAN-21. 
+Further investigation is needed to locate the correct repository or file.
+*/
