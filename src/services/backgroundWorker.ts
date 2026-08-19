5bcdd6dc9413c273a1305ecdba585596b4deff21
pvaranasi-winfo <phaneendra.varanasi@winfosolutions.com>
2026-08-19 11:10:31 +0530

    feat: Add placeholder for background worker memory leak fix [KAN-17]

---
+++ src/services/backgroundWorker.ts
@@ -0,0 +1,3 @@
+// TODO: Implement fix for memory leak in attachmentCache
+// The attachmentCache in the background worker service needs to be cleared periodically to prevent OOMKills.
+// The relevant code for the background worker service and attachmentCache could not be located in this repository.
